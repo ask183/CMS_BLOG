bdc5d4228ce297e526148215beb70e650ce692b0
ask183 <aabharan.koppal@gmail.com>
2022-09-09 21:09:42 -0400

    reactjs blog graphql

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,13 @@
-import Head from 'next/head'
-import { Global } from '@emotion/react'
-import xw from 'xwind'
+import React, {useEffect, useState } from 'react';
+import { Layout } from '../components';
+import 'tailwindcss/tailwind.css';
+import '../styles/global.scss';
 
 function App({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <title>Tailwindcss Emotion Example</title>
-      </Head>
-      <Global
-        //tailwind base styles + keyframes + ring and shadow classes variables  ... to global styles
-        styles={xw`XWIND_BASE XWIND_GLOBAL`}
-      />
+  <Layout>
       <Component {...pageProps} />
-    </>
+  </Layout>
   )
 }
 
